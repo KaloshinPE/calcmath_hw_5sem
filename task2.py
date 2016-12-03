a6e7ab50627a18957936e18fcbe5ed690d98d897
Pavel <pavelkaloshin@gmail.com>
2016-12-04 01:54:47 +0300

    Jacobi method implemented

--- task2.py
+++ task2.py
@@ -1,12 +1,13 @@
 # the task is to solve the system of linear equations via using three different methods: direct, iterative and variation
 # I suppose that system has the only one solution
 import numpy as np
-
+# parameters
+number_of_iterations = 43
 
 # input system
-A_start = np.array([[1.0, 2.0, 3.0],
-                    [4.0, 5.0, 6.0],
-                    [7.0, 8.0, 11.0]])
+A_start = np.array([[11.0, 2.0, 3.0],
+                    [4.0, 15.0, 6.0],
+                    [7.0, 8.0, 21.0]])
 b_start = np.array([10.0, 11.0, 12.0])
 
 
@@ -50,33 +51,56 @@ for i in range(A.shape[0]-1):
     exchange_lines(i, find_nonzero_j(i, i))
     map(lambda j: add_line(j, i, -1*A[j][i]/A[i][i]), range(A.shape[0])[i+1:])
 
-print "direct method:" + str(solve_triangular_system(A, b))
+direct_solution = solve_triangular_system(A, b)
+print "direct method:" + str(direct_solution)
 
 
-''' method of simple iteration (iterative)'''
+''' method Jacobi (iterative)'''
 A = np.copy(A_start)
 b = np.copy(b_start)
 
-def matrix_norm(matrix):
-    # calculates ||A||1 norm of matrix
-    max = 0.0
-    for i in range(matrix.shape[0]):
-        new = 0
-        for k in range(matrix.shape[0]):
-            new += np.abs(matrix[i][k])
-        # new = reduce(lambda j1, j2: np.abs(matrix[i][j1]) + np.abs(matrix[i][j2]), range(matrix.shape[0]))
-        if (new > max): max = new
-    return max
 
-# tau = 0.063
-tau = 0
-R = np.identity(A.shape[0]) - tau*A
-print matrix_norm(R)
-F = tau*b
-x = np.zeros(A.shape[0]) # start approximation
-for i in range(1000):
-    x = np.dot(R, x) + F
-print "interative method: " + str(x)
+def check_covergence(matrix):
+    for i in range(matrix.shape[0]):
+        summ = 0
+        for j in range(matrix.shape[0]):
+            if j!=i:
+                summ += matrix[i][j]
+        if np.abs(matrix[i][i] <= summ):
+            print "condition for covergence is not sutisfied"
+            return False
+    return True
+
+
+def reverse_matrix(matrix):
+    # calculates revers matrix to diagonal matrix
+    reversed_matrix = np.copy(matrix)
+    for i in range(matrix.shape[0]):
+        reversed_matrix[i][i] = 1/reversed_matrix[i][i]
+    return reversed_matrix
+
+
+if check_covergence(A) :
+    L, D, U = np.zeros(A.shape), np.zeros(A.shape), np.zeros(A.shape)
+    for i in range(A.shape[0]):
+        for j in range(A.shape[0]):
+            if i == j:
+                D[i][j] = A[i][j]
+            elif i > j:
+                L[i][j] = A[i][j]
+            elif i<j:
+                U[i][j] = A[i][j]
+
+    R = -1*np.dot(reverse_matrix(D), L+U)
+    F = np.dot(reverse_matrix(D), b)
+    x = np.zeros(A.shape[0])
+
+    for i in range(number_of_iterations):
+        x = np.dot(R, x) + F
+
+    print "iterative method: " + str(x) + ", number of iterations: " + str(number_of_iterations) + ", accuracy = " + str(np.linalg.norm(direct_solution - x))
+
+
 
 
 
